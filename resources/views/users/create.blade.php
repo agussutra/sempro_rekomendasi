a7ce63a625a72b5c4134e84f6a3dbd696aa4d25f
jikdez <ajikdezz@gmail.com>
2024-09-10 20:25:34 +0800

    create

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -131,11 +131,11 @@
                             <table class="table tableLagu" id="tableLagu">
                                 <thead>
                                     <tr>
-                                        <th>Artist</th>
-                                        <th>Song</th>
-                                        <th>Valency</th>
-                                        <th>Energy</th>
-                                        <th>Action</th>
+                                        <th>Artis</th>
+                                        <th>Lagu</th>
+                                        <th>Valensi</th>
+                                        <th>Energi</th>
+                                        <th>Aksi</th>
                                     </tr>
                                 </thead>
                                 <tbody></tbody>
@@ -211,8 +211,10 @@
                     <tr>
                         <td>${item.artists[0].name}</td>
                         <td>${item.name}</td>
-                        <td>
-                            <button type="button" class="btn btn-primary btn-sm" data-item='${JSON.stringify(item)}' onclick="return saveTrack(this)">+</button>
+                        <td style="text-align:center">
+                            <button type="button" class="btn btn-primary btn-sm" data-item='${JSON.stringify(item)}' onclick="return saveTrack(this)"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16">
+                            <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/>
+                            </svg></button>
                         </td>
                     </tr>
                 `).join('')}
